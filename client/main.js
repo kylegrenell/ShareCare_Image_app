5620490111e12bf7375a5d6847e2b0d0cc920235
Kyle Grenell <kyle.grenell@gmail.com>
2017-02-19 22:08:55 +0000

    iamges displaying with loop in db, delete functionality added

--- client/main.js
+++ client/main.js
@@ -3,37 +3,25 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-// Images = new Mongo.Collection("images");
+Images = new Mongo.Collection("images");
 // console.log( Images.find().count() );
 
 if(Meteor.isClient){
   console.log("I am the client");
-  let img_data = [
-  {
-    img_src: "tove.jpg",
-    img_alt: "moomin"
-  },
-  {
-   img_src: "moomin2.jpg",
-   img_alt: "moomin 2"
- },
- {
-   img_src: "moomin3.jpg",
-   img_alt: "moomin 3"
- }
+  
 
- ];
-
- Template.images.helpers(
- {
-  images: img_data
-}
-);
+ // Template.images.helpers({images: img_data});
+ Template.images.helpers({images: Images.find()});
 
  Template.images.events({
   'click .js-image': function(e){
     // console.log(e);
     $(event.target).css("width", "50px");
+  },
+
+  'click .js-del-image':function(e){
+    var image_id = this._id;
+    console.log(image_id);
   }
 });
 
--- server/main.js
+++ server/main.js
@@ -5,29 +5,18 @@ console.log( Images.find().count() );
 
 if(Meteor.isServer){
 
-Meteor.startup(() => {
+  Meteor.startup(() => {
   // code to run on server at startup
   console.log("I am the server")
 
-    if(Images.find().count() == 0){
-      Images.insert(
-        {
-          img_src: "tove.jpg",
-          img_alt: "moomin"
-        }
-        );
-      Images.insert(
-        {
-          img_src: "moomin2.jpg",
-          img_alt: "moomin 2"
-        }
-        );
-      Images.insert(
-      {
-        img_src: "moomin3.jpg",
-        img_alt: "moomin 3"
-      }
-      );
-    }
-  });
+  if(Images.find().count() == 0){
+
+  for(var i = 1; i < 23; i++){
+    Images.insert({
+      img_src: "img_" + i + ".jpg",
+      img_alt: "image number" + i
+    });
+  } 
+}
+});
 }
