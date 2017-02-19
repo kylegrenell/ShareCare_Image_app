d2221ce30bea751e76062c64c7d982c84bfe2d90
Kyle Grenell <kyle.grenell@gmail.com>
2017-02-19 17:18:53 +0000

    image helper array puts objects into html

--- client/main.js
+++ client/main.js
@@ -5,5 +5,26 @@ import './main.html';
 
 if(Meteor.isClient){
   console.log("I am the client");
+  let img_data = [
+  {
+    img_src: "tove.jpg",
+    img_alt: "moomin",
+    height: "300px"
+  },
+ {
+   img_src: "moomin2.jpg",
+   img_alt: "moomin 2",
+   height: "400px"
+ },
+ {
+   img_src: "moomin3.jpg",
+   img_alt: "moomin 3",
+   height: "400px"
+ }
+
+  ];
+
+  Template.images.helpers({images: img_data});
+
 }
 
