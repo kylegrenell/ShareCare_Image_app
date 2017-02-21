710804ee64e901afafe26764c2337768f872f86f
Kyle Grenell <kyle.grenell@gmail.com>
2017-02-21 19:45:13 +0000

    landing page with route to main, ability to access image individually and return to main gallery

--- server/main.js
+++ server/main.js
@@ -1,15 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
-console.log( "Images in database: " + Images.find().count() );
-
 Meteor.startup(() => {
   console.log("Server is up and running...")
-  if(Images.find().count() == 0){
-    for(var i = 1; i < 23; i++){
-      Images.insert({
-        img_src: "img_" + i + ".jpg",
-        img_alt: "image number" + i
-      });
-    } 
+  if (Images.find().count() == 0){
+    for (var i=1;i<23;i++){
+      Images.insert(
+      {
+        img_src:"img_"+i+".jpg",
+        img_alt:"image number "+i 
+      }
+      );  
+    }
+    console.log("Images in database: " + Images.find().count());
   }
 });
